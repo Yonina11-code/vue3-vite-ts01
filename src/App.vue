d42f4866317d122847f7b3702eb35f440907789a
yangxiaotao <yangxiaotao@yuuwei.com>
2023-03-06 15:41:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import myHeader from '@/components/myHeader.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <my-header></my-header>
+      </el-header>
+      <el-main>
+        <RouterView />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+</style>
