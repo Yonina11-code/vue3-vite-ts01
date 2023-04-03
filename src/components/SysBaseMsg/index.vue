ebaa49da918b2c9e69ebfadf2d5106b470297c9d
KylinCode <861044011@qq.com>
2023-04-03 22:22:18 +0800

    update

--- src/components/SysBaseMsg/index.vue
+++ src/components/SysBaseMsg/index.vue
@@ -1,21 +1,63 @@
 <template>
   <div class="content">
-    <p class="title">个人信息</p>
+    <p class="title">个人信息</p >
     <div class="flex-justify-between">
       <div class="flex-start-center base-avatar">
         <img class="avatar" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
         <label>test</label>
       </div>
       <div class="flex-column-start base-pro">
-        <p>用户登录： LV12</p>
+        <p>用户登录： LV12</p >
         <el-progress :percentage="100" :format="format" />
       </div>
     </div>
     <el-divider />
+    <!-- 登录信息 -->
+    <div class="base-login-info">
+      <div class="base-login-info-item" v-for="item of loginArr" :key="item.id">
+        <div class="base-login-info-item-title">{{item.title}}</div>
+        <div class="base-login-info-item-content">{{item.value}}</div>
+      </div>
+    </div>
+    <!-- 最近编辑 -->
+    <div class="base-login-edit">
+      <p class="title">最近编辑</p >
+      <div class="base-login-edit-resume" v-for="item of resumeArr" :key="item.id">
+        <div class="base_login-edit-resume-title">
+          <el-icon :size="25" color="hotpink">
+            <edit />
+          </el-icon>
+          {{item.title}}
+        </div>
+        <div>{{item.time}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref,reactive } from 'vue'
+  // npm install @element-plus/icons-vue
+  // npm config set registry https://registry.npm.taobao.org
+  // npm config get registry
+  import { Edit } from '@element-plus/icons-vue'
+
+  interface loginArrObj{
+  id:number,
+  title?:string,
+    value?:string
+ }
+
+  interface resumeArrObj{
+    id:number,
+    title?:string,
+    time?:string
+  }
+
+  const loginArr = ref<loginArrObj[]>([{id:1, title:"最近登录时间", value:"2023-04-03 00:00:00"}, {id:2, title:"最近登录IP地址", value:"127.0.0.1"}, {id:3, title:"当前登录用户IP", value:"127.0.0.1"}])
+
+  const resumeArr = ref<resumeArrObj[]>([{id:1, title:"如何快速定制个性化简历?", time:"2023-04-03 00:00:00"}, {id:2, title:"如何快速定制个性化简历?", time:"2023-04-03 00:00:00"}, {id:3, title:"如何快速定制个性化简历?", time:"2023-04-03 00:00:00"}])
+
   const format = (percentage: number) => (percentage === 100 ? 'Full' : `${percentage}%`)
 </script>
 
@@ -39,6 +81,37 @@
   }
   .base-pro {
     flex: 2;
+  }
+  .base-login-info {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    .base-login-info-item {
+      display: flex;
+      flex-direction: column;
+      align-content: center;
+      .base-login-info-item-title {
+        font-size: 12px;
+        color: #ebecef;
+      }
+      .base-login-info-item-content {
+        color: #000000;
+      }
+    }
+  }
+  .base-login-edit {
+    margin-top: 10px;
+    .base-login-edit-resume {
+      display: flex;
+      margin-top: 5px;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .base_login-edit-resume-title {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+    }
   }
 }
 .el-progress--line {
