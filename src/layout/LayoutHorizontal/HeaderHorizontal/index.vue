c475ec4b31d3cf76fbb53ba95ae1f5b373c99eb7
yangxiaotao <yangxiaotao@yuuwei.com>
2023-04-25 17:27:47 +0800

    update

--- src/layout/LayoutHorizontal/HeaderHorizontal/index.vue
+++ src/layout/LayoutHorizontal/HeaderHorizontal/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <nav class="fixed-top navbar top-nav-collapse">
-    <div></div>
-    <div></div>
+  <nav class="fixed-top">
+    <el-menu
+      class="navbar top-nav-collapse"
+      :default-active="activeIndex"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="0">LOGO</el-menu-item>
+      <div class="flex-grow" />
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">Processing Center</el-menu-item>
+      <el-menu-item index="3">Processing Center</el-menu-item>
+      <li class="el-menu-item">
+        <el-button type="primary" round>Primary</el-button>
+      </li>
+    </el-menu>
   </nav>
 </template>
 
@@ -11,23 +25,35 @@
 
 <style lang="less" scoped>
 .fixed-top {
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: 1030;
+  position: sticky;
+   top: -1px;
+    background: #B8C1C8;
+  border-bottom: 1px solid #989EA4;
+  border-top: 1px solid #717D85;
+  color: #FFF;
+  font: bold 18px/21px Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 2px 0 0 12px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: -1px;
+//   right: 0;
+//   left: 0;
+//   z-index: 1030;
 }
 .navbar {
   background-color: #ffffff;
   font-weight: 600;
   line-height: 54px;
   height: 54px;
+  padding: 28px 100px;
   box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.05);
 }
 .top-nav-collapse {
-  padding-top: 28px;
-  padding-bottom: 28px;
   background-color: #ffffff;
   box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.05);
+}
+.flex-grow {
+  flex-grow: 1;
 }
 </style>
